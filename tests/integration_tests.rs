0233cbf172de4a2b08c84dbe4ad54e326020c4a9
Evan Roman <evanroman1@gmail.com>
2021-02-27 01:14:43 -0800

    adds corrected hearst
    gives slight wrong value of 76 instead of 73.

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -7,6 +7,17 @@ fn test_rssimple() {
 }
 
 #[test]
-fn test_mean() {
-    assert_eq!(hurst::utils::mean(&brown72::BROWN72.to_vec()), 39.881204287109334f64);
+fn test_rs_corrected() {
+    assert_eq!(hurst::rs_corrected(brown72::BROWN72.to_vec()), 0.73f64);
 }
+
+#[test]
+fn test_rscalc() {
+    assert_eq!(hurst::utils::rscalc(&brown72::BROWN72.to_vec()), 96.44510191365018f64);
+}
+
+#[test]
+fn test_half() {
+    assert_eq!(hurst::utils::half(&vec![0, 512, 1024], 1024), vec![0, 256, 512, 768, 1024]);
+}
+
